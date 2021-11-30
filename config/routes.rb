abe851756164dd7e4b750e1c63f91e04aee1d59c
takuya nagasawa <tsukasayanagawa474@gmail.com>
2021-11-30 20:51:48 +0900

    ルーティングusersにupdate追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'users#new'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   resources :favorites, only: [:index, :create, :destroy]
-  resources :users, only: [:new, :create, :show, :edit]
+  resources :users, only: [:new, :create, :show, :edit, :update]
   resources :sessions, only: [:new, :create, :destroy]
   resources :pictures do
     collection do
